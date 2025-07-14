24c91a63bbc658af1a19da58fbac1f9a9236ce65
Matt Koehler <matt.koehler@unosquare.com>
2025-07-14 12:55:54 -0500

    adding datetime picker

--- src/components/TimeEntryList.tsx
+++ src/components/TimeEntryList.tsx
@@ -1,14 +1,30 @@
-import { View } from "react-native";
+import { Platform, SafeAreaView, View } from "react-native";
 import { Card } from "./ui/card";
 import { Text } from "./ui/text";
 import { timeEntryStore$ } from "~/stores/timeEntryStore";
 import { observer, use$ } from "@legendapp/state/react";
 import { activityStore$ } from "~/stores/activityStore";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Button } from "./ui/button";
 
 const TimeEntryList = () => {
   const timeEntries = use$(timeEntryStore$.timeEntries);
   const activities = use$(activityStore$.activities);
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+  const inputRef = useRef(null);
+
+  const onChange = (event: any, selectedDate: any) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showDatepicker = () => {
+    setShow(true);
+  };
 
   // Local state to force re-render every second
   const [tick, setTick] = useState(0);
@@ -25,6 +41,25 @@ const TimeEntryList = () => {
 
   return (
     <View>
+      {Platform.OS === "web" ? (
+        <input ref={inputRef} type="date" />
+      ) : (
+        <SafeAreaView>
+          <Button onPress={showDatepicker}>
+            <Text>Show date picker!</Text>
+          </Button>
+          <Text>selected: {date.toLocaleString()}</Text>
+          {show && (
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode={"date"}
+              is24Hour={true}
+              onChange={onChange}
+            />
+          )}
+        </SafeAreaView>
+      )}
       <Text className="text-primary-500">Time Entries</Text>
       {timeEntries.length === 0 ? (
         <Text className="text-gray-500">No time entries found.</Text>
